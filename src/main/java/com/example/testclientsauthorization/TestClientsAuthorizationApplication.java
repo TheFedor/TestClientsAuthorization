d28e88fc7ffd1751d8b32335c02567b0fb524258
Fedor <fedorkot1888v3.0@mail.ru>
2023-07-28 12:08:45 +0300

    initial commit

---
+++ src/main/java/com/example/testclientsauthorization/TestClientsAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.example.testclientsauthorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestClientsAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestClientsAuthorizationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testclientsauthorization/TestClientsAuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testclientsauthorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestClientsAuthorizationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
